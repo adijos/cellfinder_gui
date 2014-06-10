b6e628a6825b36ae5a7fe481f6c548048ca29ce8
adijos <dragon.federation.ii@gmail.com>
2014-06-10 14:32:12 +0800

    updated gen_test to save a sqequence of files based on input successfully, yay

--- gen_test.m
+++ gen_test.m
@@ -1,7 +1,13 @@
 function gen_test(save_dir, save_fn, files)
 %% load files
-load('rand1.mat') %testing default
-%% General test
+%default loading files
+data_dir = '/home/adityajoshi/git/cellfinder_gui/data/';
+for i=1:5;
+    load([data_dir 'rand' num2str(i) '.mat']);
+end
+%TO BE ADDED for loop for loading files
+%load(files); or something like that
+%% general test
 sum = zeros(256,256,100);
 for i=1:5;
     if ~exist(['rand' num2str(i)],'var')
@@ -9,8 +15,9 @@ for i=1:5;
     end
     cur_rand = eval(['rand' num2str(i)]);
     for j=1:100;
-        sum(:,:,j) = sum(:,:,j) + cur_rand
+        sum(:,:,j) = sum(:,:,j) + cur_rand;
+        eval(sprintf('image%d%d = sum(:,:,%d);',i,j,j));
+        save([save_dir '/' save_fn num2str(i) '-' num2str(j)],['image' num2str(i) num2str(j)]); % save files to save_dir with save_fn 
     end
 end
-%% save files to save_dir with save_fn
 end
